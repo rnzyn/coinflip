5987f1704becaffc9ba9a7af72a973e6a7ad6250
Pavel Kiselyov <pavel.kiselyov@protonmail.com>
2018-02-14 08:29:05 +0700

    Chore/polish (#5)
    * Various refactoring
    
    * Various updates
    
    * Update

--- README.md
+++ README.md
@@ -4,13 +4,49 @@ Listens to Bitcoin transactions and calls Ethereum smart contract methods.
 
 # Configuration
 
-Available environment variables:
+Required parameters:
 
-* `PORT`
-* `CONTRACT_ABI_FILE`
-* `BLOCKCYPHER_API_URL`
-* `BTC_TX_CONFIRM`
-* `BTC_PUBKEYS`
-* `ETH_RPC_ADDR`
-* `ETH_ACCOUNT`
-* `ETH_CONTRACT`
+* `BTC_WALLETS` - list of space-separated BTC wallet addresses.
+* `ETH_ACCOUNT_ADDRESS` - address the call transaction should be made from.
+* `ETH_CONTRACT_ADDRESS` - address of the smart contract to call.
+
+Optional parameters:
+
+* `COINFLIP_PORT` - port to bind, defaults to `3000`.
+* `BLOCKCYPHER_BASE_URL` - [BlockCypher API](https://www.blockcypher.com/dev/bitcoin/) base URL, defaults to `https://api.blockcypher.com/v1/btc/main`.
+* `BTC_TX_CONFIRMATIONS` - number of transaction confirmations, defaults to `3`.
+* `ETH_RPC_ADDRESS` - Ethereum RPC address, defaults to `http://localhost:8545`.
+* `ETH_CONTRACT_JSON` - File with JSON interface for the contract to instantiate, defaults to `./contract.json`.
+
+# Debugging
+
+Run TestRPC from [shop-token](https://github.com/ShoppersShop/shop-token) repository:
+
+    $ yarn run testrpc
+
+Run Truffle migrations in another window:
+
+    $ truffle migrate
+
+Setup auction inside `truffle console`:
+
+    ```
+    truffle(development)> DutchAuction.deployed().then(function(instance) { auction = instance });
+    truffle(development)> auction.setupAuction(ShopToken.address, 10000, 500);
+    truffle(development)> auction.startAuction();
+    ```
+
+Start `coinflip` server:
+
+    $ node index.js
+
+Perform test request in another window:
+
+    ```
+    curl -X POST http://localhost:3000/bid \
+    -H 'Content-Type: application/json' \
+    -d '{
+        "transactionID": "f854aebae95150b379cc1187d848d58225f3c4157fe992bcd166f58bd5063449",
+        "beneficiary": "0x1aec491cc146f13f296e7115c21bc6901193240d"
+    }'
+    ```
--- index.js
+++ index.js
@@ -6,83 +6,124 @@ require('console-stamp')(console, {
   }
 });
 
-const sprintf = require("sprintf-js").sprintf;
+// Require libraries
 const express = require('express');
-const addRequestId = require('express-request-id')();
 const axios = require('axios');
 const Web3 = require('web3');
+const sprintf = require("sprintf-js").sprintf;
+const addRequestId = require('express-request-id')();
+const bodyParser = require('body-parser');
+
+// Define default parameters
+const COINFLIP_PORT = process.env.COINFLIP_PORT || 3000;
+const BLOCKCYPHER_BASE_URL = process.env.BLOCKCYPHER_BASE_URL || "https://api.blockcypher.com/v1/btc/main";
+const BTC_TX_CONFIRMATIONS = process.env.BTC_TX_CONFIRMATIONS || 3;
+const ETH_RPC_ADDRESS = process.env.ETH_RPC_ADDRESS || "http://localhost:8545";
+const ETH_CONTRACT_JSON = process.env.ETH_CONTRACT_JSON || "./contract.json";
+
+// Read Bitcoin wallets or quit
+const btcWallets = process.env.BTC_WALLETS.split(" ");
+if (btcWallets.length == 0) {
+  console.error("Please define `BTC_WALLETS` environment variable");
+  process.exit(1);
+}
 
 // Read contract ABI file or quit
-console.info("Loading contract ABI from %s", process.env.CONTRACT_ABI_FILE);
+console.info("Loading contract ABI from %s", ETH_CONTRACT_JSON);
 const fs = require('fs');
-const contractAbi = JSON.parse(fs.readFileSync(process.env.CONTRACT_ABI_FILE, 'utf8'));
+const contractJson = JSON.parse(fs.readFileSync(ETH_CONTRACT_JSON, 'utf8'));
 
-// Define pubkeys
-const btcPubKeys = process.env.BTC_PUBKEYS.split(" ");
+// Init web3 contract
+const web3 = new Web3(new Web3.providers.HttpProvider(ETH_RPC_ADDRESS));
+const contract = new web3.eth.Contract(contractJson.abi, process.env.ETH_CONTRACT_ADDRESS);
 
-// Initialize main components
-const web3 = new Web3(new Web3.providers.HttpProvider(process.env.ETH_RPC_ADDR));
-const contract = new web3.eth.Contract(contractAbi, process.env.ETH_CONTRACT);
+// Init express
 const app = express();
 app.use(addRequestId);
+app.use(bodyParser.json());
+
+function handleError(request, response, message) {
+  console.error("[%s] %s", request.id, message);
+  response.status(500).send({error: message});  
+}
 
 // Main handler
-app.post('/tx/:txID', async function (req, res) {
-  console.log("[%s] Received BTC transaction: %s", req.id, req.params.txID);
-  const requestUrl = sprintf('%s/txs/%s', process.env.BLOCKCYPHER_API_URL, req.params.txID);
+app.post('/bid', function (req, res) {
+  // Validate request body or quit
+  const transactionID = req.body.transactionID;
+  const beneficiary = req.body.beneficiary;
+
+  if (typeof transactionID == "undefined" || transactionID == "") {
+    handleError(req, res, sprintf("Invalid `transactionID` provided in request body"));
+    return;
+  }
+
+  if (!web3.utils.isAddress(beneficiary)) {
+    handleError(req, res, sprintf("Invalid `beneficiary` provided in request body"));
+    return;    
+  }
+
+  // Process API request
+  console.log("[%s] Received transactionID %s for beneficiary %s", req.id, transactionID, beneficiary);
+  const requestUrl = sprintf('%s/txs/%s', BLOCKCYPHER_BASE_URL, transactionID);
 
   axios.get(requestUrl)
     .then(function (response) {
       // Check API response
       if (response.error) {
-        const message = sprintf("BlockCypher API request error: %s", response.error);
-        console.error("[%s] %s", req.id, message);
-        res.send({error: message});
+        handleError(req, res, sprintf("BlockCypher API request error: %s", response.error));
         return;
       }
 
       // Check transaction confirmations
-      if (response.data.confirmations < process.env.BTC_TX_CONFIRM) {
-        const message = sprintf("Not enough confirmations: %d", response.data.confirmations);
-        console.error("[%s] %s", req.id, message);
-        res.send({error: message});
+      if (response.data.confirmations < BTC_TX_CONFIRMATIONS) {
+        handleError(req, res, sprintf("Not enough confirmations: %d", response.data.confirmations));
         return;
       }
 
       // Find payment in transaction outputs
-      let found = false;
+      let found = false;   
+      var ethereumTransactionID = "";
       for (let out of response.data.outputs) {
         if (out.script_type == "pay-to-pubkey-hash") {
-          if (btcPubKeys.includes(out.addresses[0])) {
+          if (btcWallets.includes(out.addresses[0])) {
             found = true;
-            console.log("[%s] Found payment: %s satoshi to %s, calling smart-contract", req.id, out.value, out.addresses[0]);
-
-            // Call smart-contract
-            contract.methods.setGreeter("Pavel").send({ from: process.env.ETH_ACCOUNT, value: out.value }, function(error, result) {
-              if (error) {
-                console.error("[%s] %s", req.id, error);
-              } else {
-                console.log("[%s] Transaction submitted: %s", req.id, result);
-              }
-            });
+            console.log("[%s] Found payment: %s satoshi to %s, calling smart contract", req.id, out.value, out.addresses[0]);
 
             // Call currency converter
             // TODO
-          }
+            const bidValue = 100000;
+
+            // Check estimate gas for smart contract method
+            contract.methods.placeBitcoinBid(beneficiary, bidValue).estimateGas({}, function(error, gasAmount) {
+              console.log("Gas amount: %s", gasAmount);
+            })
+
+            // Call smart contract method
+            const parameters = { from: process.env.ETH_ACCOUNT_ADDRESS, gas: 130000 };
+            contract.methods.placeBitcoinBid(beneficiary, bidValue).send(parameters)
+              .then(function(result) {
+                console.log("[%s] Transaction submitted: %s", req.id, result.transactionHash);
+                res.status(200).send({transactionID: result.transactionHash});       
+              })
+              .catch(function(error) {
+                handleError(req, res, sprintf("%s", error));
+                return;
+              });
+            }
         }
       }
 
       if (!found) {
-        console.error("[%s] No payments found", req.id);
+        handleError(req, res, sprintf("No payments found for transactions %s", transactionID));
+        return;
       }
-
-      res.send(req.params);        
     })
     .catch(function (error) {
       console.error("[%s] %s", req.id, error);
-      res.send({error: error});
+      res.send({error: error.message});
     })
 });
 
 // Start server
-app.listen(process.env.PORT, () => console.info('Listening on port %d', process.env.PORT));
+app.listen(COINFLIP_PORT, () => console.info('Listening on port %d', COINFLIP_PORT));
